9f363647671c6abe7d88209ed458cd938251f490
Andrea Tosato <andrea.tosato@hotmail.it>
2021-03-26 18:53:22 +0100

    Init

---
+++ src/Server/Server/Items.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Server
+{
+    public class ItemsFunctions
+    {
+        [Function("HttpTriggerCSharp1")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HttpTriggerCSharp1");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }    
+}
---
+++ src/Server/Shared/ShelveItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedModels
+{
+    public class ShelveItem //: IValidatableObject
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Quantity { get; set; }
+
+        [Required]
+        public DateTime? ExpirationTime { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    Validator.TryValidateValue(Name, validationContext, )
+        //}
+    }
+}
